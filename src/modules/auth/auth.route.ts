0b1d176014260a0111f44a1e4d6b6b6b6551fb66
raidislam <raidislam@gmail.com>
2026-09-21 17:57:35 +0600

    update

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -7,22 +7,10 @@ const router = Router();
 
 router.post("/register", validate(registerSchema), register);
 
-router.post(
-  "/login",
-  validate(loginSchema),
-  login,
-);
+router.post("/login", validate(loginSchema), login);
 
-router.post(
-  "/refresh-token",
-  validate(refreshTokenSchema),
-  refreshToken,
-);
+router.post("/refresh-token", validate(refreshTokenSchema), refreshToken);
 
-router.post(
-  "/logout",
-  validate(refreshTokenSchema),
-  logout,
-);
+router.post("/logout", validate(refreshTokenSchema), logout);
 
 export default router;
